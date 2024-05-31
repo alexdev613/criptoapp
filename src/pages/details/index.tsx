d796cd03333110df2dff74d60a3fcc3b5eb66eec
Alex Nascimento <alexjose68@hotmail.com>
2024-05-31 14:36:41 -0300

    feat: Creating page files in folders

---
+++ src/pages/details/index.tsx
@@ -0,0 +1,7 @@
+export function Details() {
+  return (
+    <div>
+      <h1>Página Detalhes da Moeda</h1>
+    </div>
+  )
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>Página 404, esta página naõ existe!</h1>
+    </div>
+  )
+}
