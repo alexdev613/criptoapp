9ecf443190a2f43717683380b8b8c25cea9dc7b3
Alex Nascimento <alexjose68@hotmail.com>
2024-06-01 00:20:59 -0300

    feat: I created a setState called input to control the form's search field, and called useNavigate from 'react-router-dom' to be able to navigate to the cryptocurrency details page with a route similar to /details/

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,30 @@
+import { useState, FormEvent } from 'react';
 import styles from './home.module.css';
 import { BsSearch } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if(input === "") return;
+
+    navigate(`/details/${input}`);
+    
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Digite o nome da moeda... Ex.: bitcoin"
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
         />
 
         <button type="submit">
