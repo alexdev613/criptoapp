4781bd724451a243b666bfea257407393852dd42
Alex Nascimento <alexjose68@hotmail.com>
2024-06-02 22:40:14 -0300

    feat: I created and styled the details page by creating a section to serve as a card for the most important information about cryptocurrency! I also created a very basic loading screen!

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { CoinProps } from '../home';
+import styles from './details.module.css';
 
 interface ResponseData {
   data: CoinProps;
@@ -17,6 +18,8 @@ export function Details() {
   const navigate = useNavigate();
 
   const [coin, setCoin] = useState<CoinProps>();
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getCoin() {
@@ -51,6 +54,7 @@ export function Details() {
           }
 
           setCoin(resultData);
+          setLoading(false);
         })
 
       } catch(err) {
@@ -60,11 +64,46 @@ export function Details() {
     }
 
     getCoin();
-  }, [cripto])
+  }, [cripto]);
+
+  if(loading || !coin ) { // se loading === true ou não tiver nada em coin
+    return(
+      <div className={styles.container} >
+        <h4 className={styles.center} >Carregando detalhes...</h4>
+      </div>
+    )
+  }
   
   return (
-    <div>
-      <h1>Página Detalhes da Moeda {cripto}</h1>
+    <div className={styles.container} >
+      <h1 className={styles.center} >{coin?.name}</h1>
+      <h1 className={styles.center} >{coin?.symbol}</h1>
+
+      <section className={styles.content} >
+        <img
+          src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`}
+          alt="Logo da moeda"
+          className={styles.logo}
+        />
+        <h1>{coin?.name} | {coin?.symbol}</h1>
+
+        <p><strong>Preço: </strong>{coin?.formattedPrice}</p>
+
+        <a>
+          <strong>Mercado: </strong>{coin?.formattedMarket}
+        </a>
+
+        <a>
+          <strong>Volume: </strong>{coin?.formattedVolume}
+        </a>
+
+        <a>
+          <strong>Mudança 24h: </strong>
+          <span className={Number(coin?.changePercent24Hr) > 0 ? styles.profit : styles.loss} >
+            {Number(coin?.changePercent24Hr).toFixed(2)}%
+          </span>
+        </a>
+      </section>
     </div>
   )
 }
